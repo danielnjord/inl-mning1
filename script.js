f7d92b857d8c2b50bb5e1423103de93979160f82
danielnjord <danielnjord@hotmail.com>
2023-11-14 15:45:41 +0100

    ändra text

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 
-// skapar en function vid namn buttonClick med hjälp utav 'querySelector' 
+// skapar en funktion vid namn buttonClick med hjälp utav 'querySelector' 
 let buttonClick = document.querySelector("#button-contact").addEventListener("click", function() 
 {
     // läser av texten med id = "email" (altså inputen)
     let emailText = document.getElementById("email").value // value gör så den får ett värde 
 
-    // detta skrivs ut när jag klickar på knappen
+    // detta skrivs ut när jag klickar på knappen (kallar på funktionen)
     console.log(emailText); 
 });
 
